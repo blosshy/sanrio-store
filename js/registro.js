1f7029ba9fcbbf941301e67d60417c1b64dfdb82
Matias Martinez <matiiasss26@gmail.com>
2022-07-07 22:16:19 -0300

    se agregaron las validaciones del evento submit al registro

--- js/registro.js
+++ js/registro.js
@@ -1,9 +1,23 @@
+/* FUNCION QUE GUARDA EN LOCALSTORAGE */
+
 function guardarLocalJs(clave, valor) {
     localStorage.setItem(clave, valor);
 }
 
+/* FUNCION QUE CALCULA LA EDAD */
 
-/* GUARDAR INFO DE LOS INPUTS EN UN ARRAY */
+function calcularEdad(fecha_nacimiento) {
+    let hoy = new Date();
+    let cumpleanos = new Date(fecha_nacimiento);
+    let edad = hoy.getFullYear() - cumpleanos.getFullYear();
+    let m = hoy.getMonth() - cumpleanos.getMonth();
+    if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
+        edad--;
+    }
+    return edad;
+}
+
+/* CLASE CREADORA DE OBJETO USUARIO */
 
 class Usuario {
     constructor(nombre, apellido, edad, email, password) {
@@ -15,13 +29,107 @@ class Usuario {
     }
 }
 
+/* ARRAY VACIO PARA GUARDAR LO QUE SE PUSHEE */
+
 let usuariosRegistrados = [];
+
+/*GET DE LOS INPUTS PARA VALIDACIONES*/
+
+const inputNombre = document.getElementById('nombre');
+const inputApellido = document.getElementById('apellido');
+const inputEdad = document.getElementById('edad');
+const inputEmail = document.getElementById('email');
+const inputPassword = document.getElementById('password');
+
+/* EVENTO REGISTRO */
 
 const registro = document.getElementById("registro");
 
 registro.addEventListener("submit", (e) => {
 
     e.preventDefault();
+
+    /*VALIDACIONES DE EVENTO SUBMIT */
+
+    let edadValidacion = calcularEdad(inputEdad.value);
+
+    if ((inputNombre.value.length == 0) || (inputApellido.value.length == 0) || (inputEdad.value.length == 0) || (inputEmail.value.length == 0) || (inputPassword.value.length == 0)) {
+        registro.remove()
+
+        let contenedor = document.getElementById("form-container");
+
+        let registroExitoso = document.createElement("div");
+
+        registroExitoso.innerHTML = `
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 d-flex flex-column align-items-center">
+                <h2 class="msj-exitoso m-5">¡Oops! Parece que has ingresado un dato incorrecto :(</h2>
+                <img src="../images/reg-fail.png" alt="Melody dudando" class="img-fluid reg-exitoso-img">
+                <a href="registro.html" class="registro-exitoso-link mt-3">→ Para volver a intentarlo click aquí ←</a>
+            </div>
+        </div>
+    </div>
+    `
+        contenedor.appendChild(registroExitoso);
+    } else if (edadValidacion < 18) {
+        registro.remove()
+
+        let contenedor = document.getElementById("form-container");
+
+        let registroExitoso = document.createElement("div");
+
+        registroExitoso.innerHTML = `
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 d-flex flex-column align-items-center">
+                <h2 class="msj-exitoso m-5">¡Oops! Parece que has ingresado un dato incorrecto :(</h2>
+                <img src="../images/reg-fail.png" alt="Melody dudando" class="img-fluid reg-exitoso-img">
+                <a href="registro.html" class="registro-exitoso-link mt-3">→ Para volver a intentarlo click aquí ←</a>
+            </div>
+        </div>
+    </div>
+    `
+        contenedor.appendChild(registroExitoso);
+    } else if ((!inputEmail.value.includes("@")) || (!inputEmail.value.includes("."))) {
+        registro.remove()
+
+        let contenedor = document.getElementById("form-container");
+
+        let registroExitoso = document.createElement("div");
+
+        registroExitoso.innerHTML = `
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 d-flex flex-column align-items-center">
+                <h2 class="msj-exitoso m-5">¡Oops! Parece que has ingresado un dato incorrecto :(</h2>
+                <img src="../images/reg-fail.png" alt="Melody dudando" class="img-fluid reg-exitoso-img">
+                <a href="registro.html" class="registro-exitoso-link mt-3">→ Para volver a intentarlo click aquí ←</a>
+            </div>
+        </div>
+    </div>
+    `
+        contenedor.appendChild(registroExitoso);
+    }else if (!inputPassword.value.match(/[0-9]/g)) {
+        registro.remove()
+
+        let contenedor = document.getElementById("form-container");
+
+        let registroExitoso = document.createElement("div");
+
+        registroExitoso.innerHTML = `
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 d-flex flex-column align-items-center">
+                <h2 class="msj-exitoso m-5">¡Oops! Parece que has ingresado un dato incorrecto :(</h2>
+                <img src="../images/reg-fail.png" alt="Melody dudando" class="img-fluid reg-exitoso-img">
+                <a href="registro.html" class="registro-exitoso-link mt-3">→ Para volver a intentarlo click aquí ←</a>
+            </div>
+        </div>
+    </div>
+    `
+        contenedor.appendChild(registroExitoso);
+    }else{
 
     const nombre = document.getElementById('nombre').value;
     const apellido = document.getElementById('apellido').value;
@@ -59,16 +167,12 @@ registro.addEventListener("submit", (e) => {
     `
     contenedor.appendChild(registroExitoso);
 
-    /* let loginInfo = JSON.parse(localStorage.getItem("usuario")); */
+     /* let loginInfo = JSON.parse(localStorage.getItem("usuario")); */
+    }
 });
 
-/*GET DE LOS INPUTS PARA VALIDACIONES*/
 
-const inputNombre = document.getElementById('nombre');
-const inputApellido = document.getElementById('apellido');
-const inputEdad = document.getElementById('edad');
-const inputEmail = document.getElementById('email');
-const inputPassword = document.getElementById('password');
+/* VALIDACIONES EVENTO BLUR */
 
 /* VALIDACION INPUT NOMBRE */
 
@@ -107,18 +211,6 @@ inputApellido.addEventListener("blur", () => {
 /* VALIDACION INPUT EDAD */
 
 inputEdad.addEventListener("blur", () => {
-
-    function calcularEdad(fecha_nacimiento) {
-        let hoy = new Date();
-        let cumpleanos = new Date(fecha_nacimiento);
-        let edad = hoy.getFullYear() - cumpleanos.getFullYear();
-        let m = hoy.getMonth() - cumpleanos.getMonth();
-        if (m < 0 || (m === 0 && hoy.getDate() < cumpleanos.getDate())) {
-            edad--;
-        }
-        return edad;
-    }
-
     let edad = calcularEdad(inputEdad.value);
     if (edad >= 18) {
         let mensaje = document.getElementById("mensaje-3");
