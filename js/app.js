50c9b1bf527bccbc64ddc593080c2375aa7e990d
Matias Martinez <matiiasss26@gmail.com>
2022-08-03 20:10:08 -0300

    se soluciono el problema con fetch

--- js/app.js
+++ js/app.js
@@ -4,23 +4,25 @@ export const renderizarProductos = async (productos) =>{
     productos.forEach(producto => {
         const contenedor = document.getElementById("swiper-container")
         let card = document.createElement("div")
-        card.innerHTML(`
-        <div class="swiper-slide d-flex flex-column justify-content-center">
+
+        card.classList.add("swiper-slide", "d-flex", "flex-column", "justify-content-center");
+
+        card.innerHTML = (`
             <div class="img-card-container">
                 <img src="${producto.img}" alt="${producto.alt}"
                 class="img-fluid card-size-img img-before">
-                <img src="${producto.img-hover}" alt="${producto.alt}"
+                <img src="${producto.imghover}" alt="${producto.alt}"
                 class="img-fluid card-size-img img-after">
             </div>
             <p class="product-name mt-3">${producto.nombre}</p>
-            <span class="link-precio">${producto.precio}</span>
+            <span class="link-precio">$${producto.precio}</span>
             <div class="pb-3">
                 <a href="#" class="links-boton">
                     <div class="boton-card">
                         <span class="link-carrito">Añadir al carro</span>
                     </div>
                 </a>
-        </div>
+            </div> 
         `)
 
         contenedor.appendChild(card);
--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ import { renderizarProductos } from "./app.js";
 document.addEventListener("DOMContentLoaded", () =>{
     (async () => {
         try {
-            const response = await fetch("./data.json");
+            const response = await fetch("js/data.json");
             const data = await response.json();
 
             await renderizarProductos(data);
         } catch (error) {
         }
     })();
-});
+}); 
